955dfab96fb6bc1484fb6f2a07e3bd38dcb98772
Alexandr Ukraintsev <sadman1337322@gmail.com>
2025-05-27 11:08:42 +0300

    ЗАДАНИЕ

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -1,5 +1,4 @@
 import { EventEmitter } from './EventEmitter';
-
 /*
 1) В методе subscribe подпишитесь на событие click с помощью EventEmitter.on(eventName, callback).
 В обработчике нужно увеличивать значение поля count на 1
@@ -7,6 +6,9 @@ import { EventEmitter } from './EventEmitter';
 В качестве callback нужно передавать тот же самый обработчик, который был передан при подписке.
  */
 export const obj = {
+    count: 0,
+    subscribe() {},
+    unsubscribe() {},
     boundHandler: null,
     clickHandler() {
         this.count++;
@@ -24,13 +26,13 @@ export const obj = {
 
 /*
 Сделайте так, чтобы метод first вызывал метод second со своими аргументами, но в обратном порядке:
-
 obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
+    first(...args) {},
     first(...args) {
-        this.second(...args.reverse());
+        return this.second(...args.reverse());
     },
     second() {
         // здесь ничего писать не нужно
